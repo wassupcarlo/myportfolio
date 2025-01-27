5414055a54e9417e098bc28d6d1a4021c039e48e
Giancarlo P <giancarlo.passariello@dawsoncollege.qc.ca>
2025-01-27 15:41:45 -0500

    Add web project to website along with some design changes

--- my-react-vite-app/src/components/About.jsx
+++ my-react-vite-app/src/components/About.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import './About.css';
 
 function About() {
     return (
         <section>
-            <h2>About</h2>
-            <img src=""></img>
-            <p>Hi my name is Giancarlo. I am a 3rd year Computer Science student. I speak Italian, English, French and a little bit of Spanish. In my free time, I like to make beats for musicians.</p>
+            <h2>About Me</h2>
+            <section className="about-section">
+                <div id="about-img-div">
+                    <img id="kennyman" src="src\assets\IMG_2769.PNG"></img>
+                </div>
+                <div id="about-desc-div">
+                    <p>Hi my name is Giancarlo. I am a 3rd year Computer Science student. I speak Italian, English, French and a little bit of Spanish. In my free time, I like to make beats for musicians.</p>
+                </div>
+            </section>
         </section>
     );
 }
--- my-react-vite-app/src/components/Projects.jsx
+++ my-react-vite-app/src/components/Projects.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import './Projects.css';
 
 function Projects() {
+    const webLanguages = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'Mocha', 'Chai', 'SuperTest', 'MongoDB', 'Express.js'];
+
     return (
         <section>
             <h2>Projects</h2>
-            <p>List your projects with descriptions and links to GitHub or other repositories.</p>
+            <section className="outer-section">
+                <h3>Vehicle Theft Crimes in Montreal</h3>
+                <p>
+                    {webLanguages.map((language, index) => (
+                        <span id="proj-languages" key={index}>
+                            {language}
+                            {index < webLanguages.length - 1 ? '    ' : ''}
+                        </span>
+                    ))}
+                </p>
+                <p>This project combines data science techniques with social research methodologies to offer a comprehensive understanding of motor vehicle theft patterns in Montreal. By examining the interplay between crime statistics and sociodemographic factors, it aims to contribute valuable insights for policymakers, law enforcement agencies, and community organizations working towards safer and more equitable urban environments.</p>
+                <a href="https://github.com/wassupcarlo/Vehicle-Theft-Crimes-in-Montreal"><img className="github-button" src="src\assets\GitHub_Logo.png" ></img></a>
+            </section>
         </section>
     );
 }
