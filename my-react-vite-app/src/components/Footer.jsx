a6fb24a93bd8750533313203b27800106e44782d
Giancarlo P <giancarlo.passariello@dawsoncollege.qc.ca>
2025-01-28 09:02:27 -0500

    Fix linkedin image

--- my-react-vite-app/src/components/Footer.jsx
+++ my-react-vite-app/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer>
           <a href="https://www.linkedin.com/in/giancarlo-passariello/">
-            <img id="social-logo" src="src\assets\In-White-128@2x.png"></img>
+            <img id="social-logo" src="/In-White-128@2x.png"></img>
           </a>
           <p>2025   Giancarlo Passariello   1.0.1</p>
         </footer>
